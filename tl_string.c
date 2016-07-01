8b0726a055ecb32a5438cf94e28b5ad2aad6e336
One2r <601941036@qq.com>
2016-07-01 04:00:37 -0700

    放弃实现tl_str_highlight，为了字符串替换，需要拷贝几百行ext/standard/string.c代码

--- tl_string.c
+++ tl_string.c
@@ -5,14 +5,3 @@
 #endif
 
 #include "php_tl_toolkit.h"
-
-/** {{{ proto string tl_str_highlight()
- */
-PHP_FUNCTION(tl_str_highlight)
-{
-    zend_string *strg;
-	strg = strpprintf(0, "tl_toolkit version=%s", PHP_TL_TOOLKIT_VERSION);
-
-	RETURN_STR(strg);
-}
-/* }}} */
--- tl_toolkit.c
+++ tl_toolkit.c
@@ -130,7 +130,6 @@ PHP_MINFO_FUNCTION(tl_toolkit)
  */
 const zend_function_entry tl_toolkit_functions[] = {
 	PHP_FE(tl_toolkit_info, NULL)
-	PHP_FE(tl_str_highlight, NULL)
 	PHP_FE_END
 };
 /* }}} */
