e04b57a1a4d5dd678326c6a3063ff4819a1a346e
One2r <601941036@qq.com>
2016-11-04 00:32:17 +0800

    a

--- tl_string.c
+++ tl_string.c
@@ -5,10 +5,33 @@
 #endif
 
 #include "php_tl_toolkit.h"
+#include "zend_API.h"
 #include "ext/standard/md5.h"
 
 #define PHP_TL_DE_AUTHCODE_DEFAULT_OP   "DECODE"
 #define PHP_TL_DE_AUTHCODE_DEFAULT_KEY  "6713wj3NZqPxPILb7MyzF2nFGc3DNoSW9yWMRA"
+
+/* {{{ tl_md5
+ */
+zend_string *tl_md5(zend_string *str,zend_bool raw_output)
+{
+    zend_string *result;
+    char md5str[33];
+    PHP_MD5_CTX context;
+    unsigned char digest[16];
+
+    md5str[0] = '\0';
+    PHP_MD5Init(&context);
+    PHP_MD5Update(&context, ZSTR_VAL(str), ZSTR_LEN(str));
+    PHP_MD5Final(digest, &context);
+    if (raw_output) {
+        return zend_string_init((char *) digest, 16,0);
+    } else {
+        make_digest_ex(md5str, digest, 16);
+       return zend_string_copy(md5str);
+    }
+}
+/* }}} */
 
 /*{{ tl_de_authcode
  */
@@ -17,14 +40,17 @@ PHP_FUNCTION(tl_authcode)
     zend_string *input;
     zend_string *operate = zend_string_init(PHP_TL_DE_AUTHCODE_DEFAULT_OP, sizeof(PHP_TL_DE_AUTHCODE_DEFAULT_OP) - 1, 0);
     zend_string *key = zend_string_init(PHP_TL_DE_AUTHCODE_DEFAULT_KEY, sizeof(PHP_TL_DE_AUTHCODE_DEFAULT_KEY) - 1, 0);
-    int expiry = 0;
+    zend_long expiry = 0;
     zend_string *output = NULL;
 
-    if (zend_parse_parameters(ZEND_NUM_ARGS(), "S|SSd",&input, &operate, &key, &expiry) == FAILURE) {
-        RETURN_NULL();
-    }
-    //zend_string *md5key = _tl_md5(key);
+    ZEND_PARSE_PARAMETERS_START(1, 4)
+        Z_PARAM_STR(input)
+        Z_PARAM_OPTIONAL
+        Z_PARAM_STR(operate)
+        Z_PARAM_STR(key)
+        Z_PARAM_LONG(expiry)
+    ZEND_PARSE_PARAMETERS_END();
 
-    RETVAL_STR(input);
+    RETURN_STR(input);
 }
 /* }}} */
