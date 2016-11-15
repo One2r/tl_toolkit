c2fdb0fe3de30817866843dd5b35fe7e46f4a1c6
One2r <601941036@qq.com>
2016-11-15 14:41:17 +0800

    t

--- tl_string.c
+++ tl_string.c
@@ -7,6 +7,7 @@
 #include "php_tl_toolkit.h"
 #include "zend_API.h"
 #include "ext/standard/md5.h"
+#include "ext/standard/base64.h"
 
 #define PHP_TL_AUTHCODE_DEFAULT_OP   "DECODE"
 #define PHP_TL_AUTHCODE_DEFAULT_KEY  "6713wj3NZqPxPILb7MyzF2nFGc3DNoSW9yWMRA"
@@ -79,15 +80,31 @@ PHP_FUNCTION(tl_authcode)
                 );
 
     }
-    zval keyac,keya_md5keyac;
+
+    zval z_keyac,z_keya_md5keyac;
     zval z_key_a,z_key_c,z_md5keyac;
     ZVAL_STR_COPY(&z_key_a, key_a);
     ZVAL_STR_COPY(&z_key_c, key_c);
-    concat_function(&keyac, &z_key_a, &z_key_c);
-    zend_string *md5keyac = tl_md5(zend_string_init(Z_STRVAL(keyac),Z_STRLEN(keyac),0),0);
+    concat_function(&z_keyac, &z_key_a, &z_key_c);
+    zend_string *md5keyac = tl_md5(zend_string_init(Z_STRVAL(z_keyac),Z_STRLEN(z_keyac),0),0);
     ZVAL_STR_COPY(&z_md5keyac,md5keyac);
-    concat_function(&keya_md5keyac,&z_key_a,&z_md5keyac);
-    zend_string *cryptkey = Z_STR(keya_md5keyac);
+    concat_function(&z_keya_md5keyac,&z_key_a,&z_md5keyac);
+    zend_string *cryptkey = Z_STR(z_keya_md5keyac);
+
+    if(strcmp(ZSTR_VAL(operate), PHP_TL_AUTHCODE_DEFAULT_OP) == 0){
+        input = php_base64_decode((unsigned char*)zend_string_init(ZSTR_VAL(input)+PHP_TL_AUTHCODE_CKEY_LENGTH,ZSTR_LEN(input),0),ZSTR_LEN(input)-PHP_TL_AUTHCODE_CKEY_LENGTH);
+    }else{
+        if(expiry != 0){
+            expiry += (zend_long)time(NULL);
+        }
+        char time_str[10];
+        php_sprintf(time_str,"%010d",expiry);
+        zval z_input,z_key_b,z_input_keyb;
+        ZVAL_STR_COPY(&z_input, input);
+        ZVAL_STR_COPY(&z_key_b, key_b);
+        concat_function(&z_input_keyb, &z_input, &z_key_b);
+        zend_string *input_keyb = tl_md5(zend_string_init(Z_STRVAL(z_input_keyb),Z_STRLEN(z_input_keyb),0),0);
+    }
     RETURN_STR(cryptkey);
 }
 /* }}} */
