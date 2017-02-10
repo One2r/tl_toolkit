eac7629a05cc1f4a5dad6a59a060c86fc759ce0b
One2r <601941036@qq.com>
2017-02-10 15:50:37 +0800

    代码格式调整

--- tl_toolkit.c
+++ tl_toolkit.c
@@ -46,7 +46,7 @@ PHP_INI_END()
  */
 PHP_FUNCTION(tl_toolkit_info)
 {
-    zend_string *strg;
+  zend_string *strg;
 	strg = strpprintf(0, "tl_toolkit version=%s", PHP_TL_TOOLKIT_VERSION);
 
 	RETURN_STR(strg);
@@ -115,7 +115,7 @@ PHP_MINFO_FUNCTION(tl_toolkit)
 {
 	php_info_print_table_start();
 	php_info_print_table_header(2, "tl_toolkit support", "enabled");
-    php_info_print_table_row(2, "version", PHP_TL_TOOLKIT_VERSION);
+  php_info_print_table_row(2, "version", PHP_TL_TOOLKIT_VERSION);
 	php_info_print_table_end();
 
 	/* Remove comments if you have entries in php.ini
